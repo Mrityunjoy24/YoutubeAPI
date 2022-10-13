40941168382e256a5b034d3b68ab80d676bbae95
Mrityunjoy Kumar <mrityunjoykumar824@gmail.com>
2022-10-14 00:52:47 +0530

    some changes in pagination logic

--- controllers/fetchVideosFromDBController.js
+++ controllers/fetchVideosFromDBController.js
@@ -17,28 +17,29 @@ let fetchAllVideos = async (req, res) => {
         if (!limit) {
             limit = config.LIMIT;
         }
-
+        let tempLimit = limit + 1;
         let allVideos;
         // if next page is not given search by current time
         if (!nextPage) {
             let currentTime = new Date().toISOString();
             //sort by publishTime and then by _id
-            allVideos = await Video.find({ publishTime: { $lte: currentTime } }).sort({ "publishTime": -1, "_id": -1 }).limit(limit).exec();
+            allVideos = await Video.find({ publishTime: { $lte: currentTime } }).sort({ "publishTime": -1, "_id": -1 }).limit(tempLimit).exec();
         }
         else {
-             //sort by publishTime and then by _id and find all data 
-            allVideos = await Video.find({ _id: { $lt: nextPage } }).sort({ "publishTime": -1, "_id": -1 }).limit(limit).exec();
+            //sort by publishTime and then by _id and find all data 
+            allVideos = await Video.find({ _id: { $lte: nextPage } }).sort({ "publishTime": -1, "_id": -1 }).limit(tempLimit).exec();
         }
 
         let response = {};
-        response.data = allVideos;
         response.prevPage = allVideos[0]._doc._id;
-        if (allVideos.length < 5) {
+        if (allVideos.length < tempLimit) {
             response.hasNextPage = false
+            response.data = allVideos;
         }
         else {
             response.hasNextPage = true
             response.nextPage = allVideos[allVideos.length - 1]._doc._id;
+            response.data = allVideos.slice(0, allVideos.length - 1);
         }
 
         res.status(200).json(response);
